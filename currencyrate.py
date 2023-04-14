20b15c95c7acb9c11136c4b8c7ce074c013aaf43
Bibekkun <bibekrai655@gmail.com>
2023-04-14 12:28:59 +0545

    done

--- currencyrate.py
+++ currencyrate.py
@@ -1,5 +1,6 @@
 import requests
 from requests.api import head
+import pandas as pd
 
 url='https://www.freeforexapi.com/api/live?pairs=EURGBP,USDJPY,EURUSD,GBPUSD,AUDUSD,USDCHF,NZDUSD,USDCAD,USDZAR'
 
@@ -9,4 +10,14 @@ headers={
 }
 response=requests.request("GET",url,headers=headers)
 myjson=response.json()
+currencyrate=[]
+for pair in myjson['rates']:
+    data={
+        "Pair": pair,
+        "Rate": myjson['rates'][pair]['rate'],
+        "Timestamp": myjson['rates'][pair]['timestamp']
+    }
+    currencyrate.append(data)
+df=pd.DataFrame(currencyrate)
+df.to_csv('Currency Rate.csv',index=True)
 print(myjson)
